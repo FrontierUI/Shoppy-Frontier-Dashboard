a878a9ccd80070ef67e63ea3e45041e5a841ae42
FrontierUI <92513151+FrontierUI@users.noreply.github.com>
2022-08-18 13:52:11 +0500

    Add files via upload

---
+++ src/components/Charts/Stacked.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+
+import { useStateContext } from "../../context/ContextProvider";
+
+const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: "white" }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+    >
+      <Inject services={[Category, Legend, StackingColumnSeries, Tooltip]} />
+
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
+
+export default Stacked;
---
+++ src/components/ThemeSettings.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsCheck } from "react-icons/bs";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { themeColors } from "../data/dummy";
+import { useStateContext } from "../context/ContextProvider";
+
+const ThemeSettings = () => {
+  const { setColor, setMode, currentMode, currentColor, setThemeSettings } =
+    useStateContext();
+
+  return (
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen bg-white w-400 dark:text-gray-200 dark:bg-[#484B52]">
+        <div className="flex justify-between items-center p-4 ml-4">
+          <p className="font-semibold text-lg">Settings</p>
+
+          <button
+            type="button"
+            className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
+            onClick={() => setThemeSettings(false)}
+            style={{ color: "rgb(153, 171, 180)", borderRadius: "50%" }}
+          >
+            <MdOutlineCancel />
+          </button>
+        </div>
+
+        <div className="p-4 ml-4 flex-col border-t-1 border-color">
+          <p className="font-semibold text-xl ">Theme Option</p>
+
+          <div className="mt-4">
+            <input
+              type="radio"
+              id="light"
+              name="theme"
+              value="Light"
+              className="cursor-pointer"
+              onChange={setMode}
+              checked={currentMode === "Light"}
+            />
+
+            <label htmlFor="light" className="ml-2 text-md cursor-pointer">
+              Light
+            </label>
+          </div>
+
+          <div className="mt-2">
+            <input
+              type="radio"
+              id="dark"
+              name="theme"
+              value="Dark"
+              className="cursor-pointer"
+              onChange={setMode}
+              checked={currentMode === "Dark"}
+            />
+
+            <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
+              Dark
+            </label>
+          </div>
+        </div>
+
+        <div className="p-4 border-t-1 border-color ml-4">
+          <p className="font-semibold text-xl ">Theme Colors</p>
+
+          <div className="flex gap-3">
+            {themeColors.map((item, index) => (
+              <TooltipComponent
+                key={index}
+                position="TopCenter"
+                content={item.name}
+              >
+                <div
+                  className="mt-2 relative flex gap-5 cursor-pointer items-center"
+                  key={item.name}
+                >
+                  <button
+                    type="button"
+                    className="h-10 w-10 rounded-full cursor-pointer"
+                    onClick={() => setColor(item.color)}
+                    style={{ backgroundColor: item.color }}
+                  >
+                    <BsCheck
+                      className={`ml-2 text-2xl text-white ${
+                        item.color === currentColor ? "block" : "hidden"
+                      }`}
+                    />
+                  </button>
+                </div>
+              </TooltipComponent>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThemeSettings;
